5509d5723ee75bf47ddee1ead43ebba56ea556b4
Mark Friday <davidmarkfriday16@gmail.com>
2023-03-09 21:34:59 +0100

    Page Header or Top Navigation Bar completed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     
-    //When this function is called, adjust the position and width of the Bottom 
-    //Bar to be same with the element in the parameter
+    //  When this function is called, adjust the position and width of the Bottom 
+    //  Bar to be same with the element in the parameter
     function adjustBottomBar(item) {
 
         var bottomBar = document.querySelector(".bottom-bar");
@@ -14,31 +14,31 @@ window.onload = function () {
     }
 
 
-    //Get the currently active Nav Item
+    //  Get the currently active Nav Item
     var activeNavItem = document.querySelector(".nav-bar ul li a.active");
-    //Call this function and pass the currently active Nav Item
-    //as an argument, to adjust and make the width and position 
-    //of our bottom bar same as the active Nav Item
+    //  Call this function and pass the currently active Nav Item
+    //  as an argument, to adjust and make the width and position 
+    //  of our bottom bar same as the active Nav Item
     adjustBottomBar(activeNavItem);
 
-    //Get all the Nav Items,
+    //  Get all the Nav Items,
     var navItems = document.querySelectorAll("nav ul li a");
 
-    //Attach some event listeners to them
+    //  Attach some event listeners to them
     navItems.forEach(navItem => {
 
-        //When any of the item is hovered on, call the 
-        //adjustBottomBar() function and pass the currently
-        //hovered item as an argument, to enable us set the
-        //position and width property of the Bottom Bar to 
-        //be same as the currently hovered element.
+        //  When any of the item is hovered on, call the 
+        //  adjustBottomBar() function and pass the currently
+        //  hovered item as an argument, to enable us set the
+        //  position and width property of the Bottom Bar to 
+        //  be same as the currently hovered element.
         navItem.addEventListener('mouseenter', ()=> {
             adjustBottomBar(navItem);
         });
 
-        //When the mouse pointer leaves the currently hovered item,
-        //make the width and position of the Bottom Bar to be 
-        //same as the Nav Item with the active class
+        //  When the mouse pointer leaves the currently hovered item,
+        //  make the width and position of the Bottom Bar to be 
+        //  same as the Nav Item with the active class
         navItem.addEventListener('mouseleave', ()=> {
             adjustBottomBar(activeNavItem);
         });
@@ -49,13 +49,25 @@ window.onload = function () {
 
     hamburgerMenu.addEventListener("click", ()=> {
         navBar.classList.toggle("active");
+        hamburgerMenu.classList.toggle("active");
     });
 
+    //  When the device width is greater than 1065px, perform these actions
     window.addEventListener("resize", ()=> {
         if (window.innerWidth > 1065) {
             navBar.classList.remove("active");
+            hamburgerMenu.classList.remove("active");
         } 
     });
-           
     
+    //  When User clicks on the page, and not the header, the Nav Bar slides back up
+    document.onclick = function(target) {
+        var header = document.querySelector("header");
+        var isClickInsideHeader = header.contains(event.target);
+        if (isClickInsideHeader !== true){
+            navBar.classList.remove("active");
+            hamburgerMenu.classList.remove("active");
+        }
+    
+    }
 }
